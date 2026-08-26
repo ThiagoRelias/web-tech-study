b08a1a53fb60727c03317cedb21803e986911a83
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-08-26 10:23:20 -0300

    feat: adicioando Count.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import Banner from "./Components/Banner/Banner"
-import Header from "./Components/Header"
-import Footer from "./Components/Footer"
-import Card from "./Components/Card"
-
-
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Banner from "./Components/Banner/Banner";
+import Count from "./Components/Count";
 
 function App() {
   return (
     <>
       <Header title="Meu site" />
 
-      <Header title="Lorem ipsum" />
-
-      <Header title="Introdução ao React" />
-
+      <Count />
       {/* <ImgCard caption="Lorem ipsum dolor sit amet," /> */}
-
-
 
       <Banner>
         <h1>Bem-Vindo ao meu site</h1>
         <p>Aqui você encontrará conteúdos sobre desenvolvimento web.</p>
       </Banner>
-      {/* <Card /> */}
-      <Footer title="new Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
 
+      {/* <Card /> */}
+
+      <Footer title="new Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
     </>
   )
 }
