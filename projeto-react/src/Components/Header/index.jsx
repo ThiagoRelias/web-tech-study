22578432aadd04cd8a93b4c430884df65a2d1bd3
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-09-02 10:10:02 -0300

    feat: adicionando arquivos a projeto-react

---
+++ projeto-react/src/Components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <header className="header">
+        <section className="title-container">
+            <h1 className="title">Titulo Principal</h1>
+            <h2>Lorem ipsum</h2>
+        </section>
+    </header>
+  )
+}
+    
