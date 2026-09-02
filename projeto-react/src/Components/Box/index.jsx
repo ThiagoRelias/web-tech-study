608fd5eaf6d8f93dc402ef990571981df7e39f40
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-09-02 11:28:30 -0300

    feat: criando Pages/Home

--- projeto-react/src/Components/Box/index.jsx
+++ projeto-react/src/Components/Box/index.jsx
@@ -1,14 +1,24 @@
-import "./Box.css"
-import React from 'react'
+import "./Box.css";
+import React from "react";
 
 export default function index(props) {
-    console.log(props)
+  console.log(props);
+
   return (
     <section className="container-box">
-      <h1 className="title-box">{props.title}</h1>
+      <img 
+        className="image" 
+        src={props.imagem} 
+        alt={props.title} 
+      />
+
+      <h1 className="title-box">
+        {props.title}
+      </h1>
+
       <p className="description">
-      {props.description}
-        </p>
+        {props.description}
+      </p>
     </section>
-  )
+  );
 }
--- projeto-react/src/Pages/Home/index.jsx
+++ projeto-react/src/Pages/Home/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Box from '../../Components/Box';
-import Img1 from "../../assets/img/box-1.jpg"
-import Img2 from "../../assets/img/box-2.jpg"
+import Img1 from "../../assets/img/box-1.jpg";
+import Img2 from "../../assets/img/box-2.jpg";
+
+
 
 export default function index() {
   return (
     <main className="container">
-      <section>
-        <Box 
+      <section className="d-flex">
+        <Box
           title="Titulo do componente"
           description="Este é um parágrafo de amplo para o componente."
           imagem={Img1}
+        />
+        <Box
+          title="Titulo do componente"
+          description="Este é um parágrafo de amplo para o componente."
+          imagem={Img2}
         />
       </section>
     </main>
